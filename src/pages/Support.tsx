399bf66f6804dfb7d5c23b0487c1a5a271536ddb
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-06 17:13:45 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -69,7 +69,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('https://wa.me/2348012345678')}
+              onClick={() => window.open('https://wa.me/2348189668037')}
             >
               Chat on WhatsApp
             </Button>
