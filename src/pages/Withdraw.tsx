61b3241bf065c9675cbe16f4e6b76afbf4b1e0b6
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-08 22:45:22 +0100

    Update Withdraw.tsx

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -73,7 +73,7 @@ const Withdraw = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "BPC1515177") {
+    if (bpcCode !== "BPC65747") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
