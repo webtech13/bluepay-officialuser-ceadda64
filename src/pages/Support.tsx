9210bd4f8bfa3c9ad5f83dc428a86d29045240cd
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-27 13:56:33 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -37,7 +37,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('mailto:bluepaycustomerservice25@gmail.com')}
+              onClick={() => window.open('mailto:emmanuelanaelechi90@gmail.com')}
             >
               Contact via Email
             </Button>
@@ -55,7 +55,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('https://t.me/Officialbluepay')}
+              onClick={() => window.open('https://t.me/officialbluepay28')}
             >
               Open Telegram
             </Button>
@@ -100,7 +100,7 @@ const Support = () => {
 
         <div className="mt-8 text-center">
           <p className="text-gray-500">Available 24/7 for your support needs</p>
-          <p className="text-blue-600 font-medium mt-2">bluepaycustomerservice25@gmail.com</p>
+          <p className="text-blue-600 font-medium mt-2">emmanuelanaelechi90@gmail.com</p>
         </div>
       </div>
     </div>
