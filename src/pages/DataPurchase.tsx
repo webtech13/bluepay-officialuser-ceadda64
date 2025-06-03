957d9c662d6c903bd7fa33ec7f8d13710f121f1b
Linerty17 <sundaychinemerem66@gmail.com>
2025-06-03 15:18:56 +0100

    Update DataPurchase.tsx

--- src/pages/DataPurchase.tsx
+++ src/pages/DataPurchase.tsx
@@ -56,7 +56,7 @@ const DataPurchase = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "BPC7319111") {
+    if (bpcCode !== "BPC11666") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
