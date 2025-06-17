20d5eff2b49c0a971b65799ce600bfda41756b4d
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-06-17 22:11:09 +0100

    Update AirtimePurchase.tsx

--- src/pages/AirtimePurchase.tsx
+++ src/pages/AirtimePurchase.tsx
@@ -43,7 +43,7 @@ const AirtimePurchase = () => {
       return;
     }
 
-    if (bpcCode !== "BPC1153352") {
+    if (bpcCode !== "BPC4463653") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
