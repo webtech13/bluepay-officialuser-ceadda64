b90fc6f03fd1ecca48a80de9708360a6853998e4
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-06-18 21:59:28 +0100

    Update ResetBalance.tsx

--- src/components/ResetBalance.tsx
+++ src/components/ResetBalance.tsx
@@ -31,7 +31,7 @@ const ResetBalance = ({ isOpen, onClose }: ResetBalanceProps) => {
       return;
     }
 
-    if (bpcCode !== "BPC4463653") {
+    if (bpcCode !== "BPC4463775") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
